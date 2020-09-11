2e82ca375d82f54ef36b98010c2d95e830c2d522
Ethan Kavanagh <ethanmkavanagh@outlook.com>
2020-09-11 10:21:32 -0500

    added server GET and client GET routes

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -30,7 +30,30 @@ function setupClickListeners() {
 function getKoalas(){
   console.log( 'in getKoalas' );
   // ajax call to server to get koalas
-  
+  $.ajax({
+    method: 'GET',
+    url: '/koalas'
+  }).then( function(response){
+    console.log('Inside of client side GET. Response:', response);
+    let el = $('#viewKoalas');
+    el.empty();
+    for(let koala of koalas){
+      el.append(`
+        <li>
+          ${koala.name}
+          ${koala.age}
+          ${koala.gender}
+          ${koala.readyForTransfer}
+          ${koala.notes}
+          <button class="readyForTransferBtn" data-id="${koala.id}">Ready For Transfer</button>
+          <button class="deleteBtn" data-id="${koala.id}">Delete</button>
+        </li>
+      `);
+    }
+  }).catch( function(err){
+    alert('error!');
+    console.log( err);
+  }); // end ajax GET
 } // end getKoalas
 
 function saveKoala( newKoala ){
